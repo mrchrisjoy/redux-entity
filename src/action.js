a15d965eb005236992ca2b3fef525480b4d81fa9
Chris Joy <chrisjoydev@gmail.com>
2018-11-25 23:39:14 +1100

    feat(action): Added action helpers

---
+++ src/action.js
@@ -0,0 +1,40 @@
+const defaultConfig = [
+  'addEntity',
+  'addEntities',
+  'removeEntity',
+  'removeEntities',
+  'removeSelectedEntity',
+  'addMeta',
+  'select',
+  'reset',
+];
+
+const parseToTitleCase = str => str
+  .replace(/([A-Z])/g, ' $1')
+  .replace(/\w\S*/g, txt => `${txt.charAt(0).toUpperCase()}${txt.substr(1).toLowerCase()}`);
+
+const parseToMacroCase = str => str
+  .replace(/([A-Z])/g, '_$1')
+  .toUpperCase();
+
+const createTypes = (namespace, config) => config.reduce((accumulator, action) => ({
+  ...accumulator,
+  [parseToMacroCase(action)]: `[${parseToTitleCase(namespace)}] ${parseToTitleCase(action)}`,
+}), {});
+
+const createCreators = (types, config) => config.reduce((accumulator, action) => ({
+  ...accumulator,
+  [action]: payload => ({ type: types[parseToMacroCase(action)], payload }),
+}), {});
+
+export const filterActionTypes = actions => Object.keys(actions)
+  .filter(action => action.match(new RegExp(/^([A-Z])+(_)?/, 'g')))
+  .reduce((accumulator, key) => ({ ...accumulator, [key]: actions[key] }), {});
+
+export const createActionsConfig = (config = []) => [...defaultConfig, ...config];
+
+export const createActions = (namespace, config = defaultConfig) => {
+  const types = createTypes(namespace, config);
+  const creators = createCreators(types, config);
+  return { types, creators };
+};
--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
 export * from './reducer';
 export * from './factory';
 export * from './selector';
+export * from './action';
